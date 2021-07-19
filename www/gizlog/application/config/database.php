83431e44f80aeca6bd87fd59316eb3e1e16c3fde
大塚大樹 <s87.otsuka.daiki@gmail.com>
2021-07-19 18:50:18 +0900

    チュートリアル終了

--- www/gizlog/application/config/database.php
+++ www/gizlog/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
+	'hostname' => 'db',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'gizlog',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
