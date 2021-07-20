f584ffa8b1657465aa7dcae4bd384acec7a9fc9b
大塚大樹 <s87.otsuka.daiki@gmail.com>
2021-07-20 15:30:06 +0900

    日報作成画面へのルートリソース定義

---
+++ www/gizlog/application/controllers/Daily_reports.php
@@ -0,0 +1,15 @@
+<?php
+
+class Daily_reports extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url_helper');
+    }
+
+    public function create ()
+    {
+        $this->load->view('templates/footer');
+    }
+}
